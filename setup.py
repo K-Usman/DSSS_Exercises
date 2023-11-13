83e2d6f19507b27d5eaad1f6d167f2731c8c60ce
UsmanAST <usman.kamran@astera.com>
2023-11-13 02:26:41 +0100

    fixed git ignore file. It was not tracking setup.py and requirements.py files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Homework1',
+    version='0.1.0',    
+    description='First python Exercise DSSS',
+    url='https://github.com/K-Usman/DSSS_Exercises',
+    author='Mohammad Usman Kamran',
+    author_email='umnkamran2@gmail.com',
+    license='Apache License',
+    packages=['mathquiz'],
+    install_requires=[                     
+                      ])
